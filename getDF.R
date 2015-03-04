eab75504e521a0e83b8618c7f3791f506474578b
phillipwhite <philwhiteuk1@gmail.com>
2015-03-04 14:00:05 +0000

    one argument per line

--- getDF.R
+++ getDF.R
@@ -9,7 +9,8 @@ getDF <- function() {
 	if (!file.exists(zipfile)) {
 		myurl <- "http://d396qusza40orc.cloudfront.net/exdata/data/household_power_consumption.zip"
 		setInternet2(use = TRUE)
-		download.file(url = myurl, destfile = zipfile)
+		download.file(url = myurl, 
+			      destfile = zipfile)
 		
 		# check download has succeeded
 		if (!file.exists(zipfile)) {
@@ -40,17 +41,24 @@ getDF <- function() {
 		# read the txtfile filtered to have only those rows
 		#    where the Date is 1/2/2007 or 2/2/2007
 		sqlstr <- "SELECT * FROM file WHERE Date IN (1/2/2007', '2/2/2007')"
-		df <- read.csv.sql(txtfile, sql = sqlstr, 
-				   header = TRUE,  sep = ";")
+		df <- read.csv.sql(file = txtfile, 
+				   sql = sqlstr, 
+				   header = TRUE,  
+				   sep = ";")
 		closeAllConnections()
 		
 		# save the filtered data to short_textfile
-		write.table(df, file = short_txtfile, sep = ";", row.names= FALSE)		
+		write.table(x = df, 
+			    file = short_txtfile, 
+			    sep = ";", 
+			    row.names= FALSE)		
 		
 	} else {		
 		# read the filtered data from short_textfile
-		df <- read.table(file = short_txtfile, sep=";", 
-				 header=TRUE, stringsAsFactors = FALSE)			
+		df <- read.table(file = short_txtfile, 
+				 sep=";", 
+				 header=TRUE, 
+				 stringsAsFactors = FALSE)			
 		
 	} 
 	
--- plot1.R
+++ plot1.R
@@ -9,7 +9,7 @@ plot1 <- function() {
 	with(getDF(), {   # read in the data
 		
 		# then plot the histogram with the data
-		hist(Global_active_power,
+		hist(x = Global_active_power,
 		     main = "Global Active Power",
 		     xlab = "Global Active Power (kilowatts)",
 		     ylab = "Frequency", 
