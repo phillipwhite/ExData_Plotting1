ba0ba07c22b4af4c26bf5f727ed05aa4947e7cd8
phillipwhite <philwhiteuk1@gmail.com>
2015-03-05 07:44:50 +0000

    divided getDF code to separate out downloafFile()

--- getDF.R
+++ getDF.R
@@ -1,10 +1,16 @@
-#library(data.table) 
 library(sqldf)
 
-
-getDF <- function() {
+# Function  to download, if necessary, and read in the data
+#   filtered just for the dates 1/2/2007 and 2/2/2007
+#
+# Arguments: none
+#
+# Returns: a data frame with the data and an additional column for datetime
+#
+downloadFile <- function() {
 	
 	# download the zipfile from the web, if necessary
+	# from http://d396qusza40orc.cloudfront.net/exdata/data/household_power_consumption.zip
 	zipfile <- "household_power_consumption.zip"
 	if (!file.exists(zipfile)) {
 		myurl <- "http://d396qusza40orc.cloudfront.net/exdata/data/household_power_consumption.zip"
@@ -30,15 +36,25 @@ getDF <- function() {
 			return	
 		}
 	}
+}
 
-	# read in the filtered data from short_textfile
+
+getDF <- function() {	
+	txtfile <- "household_power_consumption.txt"
+	
+	if (!file.exists(txtfile)) {
+		downloadFile()
+	}
+	
+
+	# prepare to read in the filtered data from short_textfile
 	short_txtfile <- paste("short_", txtfile, sep = "")
 	
 	# if the short_txtfile has not already been created,
 	if (!file.exists(short_txtfile)) {
 		# then create short_txtfile
 		
-		# read the txtfile filtered to have only those rows
+		# read the txtfile, filtered to have only those rows
 		#    where the Date is 1/2/2007 or 2/2/2007
 		sqlstr <- "SELECT * FROM file WHERE Date IN (1/2/2007', '2/2/2007')"
 		df <- read.csv.sql(file = txtfile, 
@@ -51,15 +67,13 @@ getDF <- function() {
 		write.table(x = df, 
 			    file = short_txtfile, 
 			    sep = ";", 
-			    row.names= FALSE)		
-		
+			    row.names = FALSE)			
 	} else {		
-		# read the filtered data from short_textfile
+		# read the filtered data directly from short_textfile
 		df <- read.table(file = short_txtfile, 
-				 sep=";", 
-				 header=TRUE, 
+				 sep = ";", 
+				 header = TRUE, 
 				 stringsAsFactors = FALSE)			
-		
 	} 
 	
 	# add a column for datetime, using Date and Time
