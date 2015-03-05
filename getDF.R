d6841716a67072f89b54ef6c5797cf8e60844392
phillipwhite <philwhiteuk1@gmail.com>
2015-03-05 19:41:33 +0000

    pasted getDF code into plot1.R. added comment headers

--- getDF.R
+++ getDF.R
@@ -2,15 +2,11 @@ library(sqldf)
 
 # Function  to download, if necessary, and read in the data
 #   filtered just for the dates 1/2/2007 and 2/2/2007
-#
 # Arguments: none
-#
-# Returns: a data frame with the data and an additional column for datetime
+# Returns: TRUE if download and unzip succeeded
 #
 downloadFile <- function() {
-	
 	# download the zipfile from the web, if necessary
-	# from http://d396qusza40orc.cloudfront.net/exdata/data/household_power_consumption.zip
 	zipfile <- "household_power_consumption.zip"
 	if (!file.exists(zipfile)) {
 		myurl <- "http://d396qusza40orc.cloudfront.net/exdata/data/household_power_consumption.zip"
@@ -20,8 +16,8 @@ downloadFile <- function() {
 		
 		# check download has succeeded
 		if (!file.exists(zipfile)) {
-			print("Warning in plot1.R getDF() - http download failed")
-			return
+			print("Warning in plot1.R downloadFile() - http download failed")
+			return(FALSE)
 		}
 	}
 	
@@ -32,25 +28,30 @@ downloadFile <- function() {
 		
 		# check unzip has succeeded
 		if (!file.exists(txtfile)) {
-			print("Warning in plot1.R getDF() - unzip failed")
-			return	
+			print("Warning in plot1.R downloadFile() - unzip failed")
+			return(FALSE)
 		}
 	}
+	return(TRUE)
 }
 
 
+# Function to read in the data filtered just for the dates 1/2/2007 and 2/2/2007
+# Arguments: none
+# Returns: a data frame with the data and an additional column for datetime
+#
 getDF <- function() {	
 	txtfile <- "household_power_consumption.txt"
 	
+	# if the file is not found, then download it
 	if (!file.exists(txtfile)) {
-		downloadFile()
+		success <- downloadFile()
 	}
-	
 
 	# prepare to read in the filtered data from short_textfile
 	short_txtfile <- paste("short_", txtfile, sep = "")
 	
-	# if the short_txtfile has not already been created,
+	# if the short_txtfile has not already been created, 
 	if (!file.exists(short_txtfile)) {
 		# then create short_txtfile
 		
